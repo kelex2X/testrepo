0f226d1b147aa6f144b480a9019ff7983bbb099d
kelex2X <elek.gabor@yahoo.com>
2022-05-12 11:49:15 +0200

    FOR IN és FOR OF gyakorlása

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let voteAge = 18;
 )*/
 
 //Rövidebben
-(voteAge <= age) ? console.log('szavazhatsz!'): console.log('nem szavazhatsz!') 
+(voteAge <= age) ? console.log('szavazhatsz!') : console.log('nem szavazhatsz!')
 
 //Compound operator
 let a = 11;
@@ -22,45 +22,45 @@ let credit = 15;
 console.log(credit *= credit > 28 ? 2 : 3)
 
 //Összegző algoritmus
-let numberArray = [1,2,3,5,8,13,21];
+let numberArray = [1, 2, 3, 5, 8, 13, 21];
 let storage = 0;
 
-for (let i= 0; i < numberArray.length; i++) {
+for (let i = 0; i < numberArray.length; i++) {
     storage += numberArray[i];
 }
 
 console.log("A tömb összege: " + storage);
 
 //Számláló algoritmus
-let numberTwoArray = [1,2,3,5,8,12,20];
+let numberTwoArray = [1, 2, 3, 5, 8, 12, 20];
 let piece = 0;
 
-for (let i=0; i < numberTwoArray.length; i++) {
+for (let i = 0; i < numberTwoArray.length; i++) {
     /*if (numberTwoArray[i] % 2 == 0) {
         piece++
     }*/
-    
-    ( numberTwoArray[i] % 2 == 0) ? piece++ : console.log()
+
+    (numberTwoArray[i] % 2 == 0) ? piece++ : console.log()
 }
 
 console.log("A tömbben lévő páros számok darabszáma: " + piece);
 
 //Szélsőérték algoritmusa
-let numberThreeArray = [1,2,3,150,100,11,20];
+let numberThreeArray = [1, 2, 3, 150, 100, 11, 20];
 let biggest = numberThreeArray[0];
 
 for (let i = 0; i < numberThreeArray.length; i++) {
-   if (numberThreeArray[i] > biggest) {
+    if (numberThreeArray[i] > biggest) {
         biggest = numberThreeArray[i];
-   }
-   
-   // ( numberThreeArray[i] > biggest) ? biggest = numberThreeArray[i] : console.log()
+    }
+
+    // ( numberThreeArray[i] > biggest) ? biggest = numberThreeArray[i] : console.log()
 }
 
 console.log("A tömbben lévő legnagyobb szám: " + biggest);
 
 //Eldöntés algoritmusa
-let numberFourArray = [1,22,5,150,51,11,20];
+let numberFourArray = [1, 22, 5, 150, 51, 11, 20];
 let contains = "hamis";
 
 for (let i = 0; i < numberFourArray.length && contains == "hamis"; i++) {
@@ -69,23 +69,23 @@ for (let i = 0; i < numberFourArray.length && contains == "hamis"; i++) {
     }
 }
 
-console.log("A tömbben megtalálható az 5: " + contains); 
+console.log("A tömbben megtalálható az 5: " + contains);
 
 //OBJEKTUMOk
 let user = {
-    name : "John Doe",
+    name: "John Doe",
     age: 22,
     city: "New York"
 }
 
 user = {
-    name : "piri",
+    name: "piri",
     age: 33,
     city: "Washington"
 }
 
 const userNew = {
-    name : "John Doe",
+    name: "John Doe",
     age: 22,
     city: "New York"
 }
@@ -100,9 +100,38 @@ console.log(userNew.name);
 console.log(userNew.name = "Béla báá");
 console.log(userNew);
 
+let number = [1, 2, 3, 5, 8, 12, 20];
 
-let number = [1,2,3,5,8,12,20];
+for (let k in userNew) {
+    console.log("kulcs: " + k + " érték: " + userNew[k]);
+}
 
 for (let k in number) {
-    console.log("kulcs: " + k + " érték: " + number[k]);
+    console.log(number[k]);
+}
+
+console.log("6. elem: " + number[5]);
+console.log("User neve: " + userNew["name"] + " " + userNew.name + "Ez e user neve: ${user.name}");
+
+const userFuck = { name: 'Jim', age: 33 }
+console.log(`Hello, my name is ${userFuck.name} and I'm ${userFuck.age} age old.`);
+
+const weather = {
+    type: 'Rainy', 
+    uv: 'low' 
+};
+for (const k in weather) {
+    console.log(`a kulcs neve: ${k} és értéke: ${weather[k]}`);
+}
+
+for (const k of Object.keys(weather)) {
+    console.log(k);
+}
+
+for (const k of Object.values(weather)) {
+    console.log(k);
+}
+
+for (const k of Object.entries(weather)) {
+    console.log(k);
 }
